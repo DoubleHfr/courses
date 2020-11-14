f5b6c5e880715d8c4167f9279a88039c10303b85
DoubleHfr <hugo.bruneau@gmail.com>
2020-11-14 16:41:41 +0100

    Created a mad libs game !

---
+++ mad_libs_game.py
@@ -0,0 +1,16 @@
+"""building a mad libs game"""
+
+# Still following this course : https://www.youtube.com/watch?v=rfscVS0vtbw&t=52s&ab_channel=freeCodeCamp.org
+
+#print("Roses are red")
+#print("Violets are blue")
+#print("I love u")
+
+#TODO Make the user turns the classic poem into something fun with some words he choses
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+
+print("Roses are "+ color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
