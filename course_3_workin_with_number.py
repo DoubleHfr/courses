54c6f25d0b41c64b6f9eba5ad2e725f7a4808aa4
DoubleHfr <hugo.bruneau@gmail.com>
2020-11-14 16:17:56 +0100

    I learn how to work with numbers in python !

---
+++ course_3_workin_with_number.py
@@ -0,0 +1,33 @@
+"""working with numbers"""
+
+#from math import * # Communly used to do advanced maths functions
+
+# I watch this course : https://www.youtube.com/watch?v=rfscVS0vtbw&t=52s&ab_channel=freeCodeCamp.org
+
+print(2) # It prints the number I wrote could be also a decimal number as 2.22208 for exemple
+print(-2) # It can also print negative numbers
+
+## Basic maths
+print(7.8 + 2.2) # You can do basic operations like that.
+print(7.8 - 2.2) # You can do basic soustractions like that.
+print(7.8 / 2.2) # You can do basic divisions like that.
+print(7.8 * 2.2) # You can do basic multiplications like that.
+
+## Complex maths
+print((7.8 + 2.2) * 3) # You can add parenthesis like that in order to change the order of the multiplication
+
+# You can store numbers in variables
+
+my_num = -5
+
+print(my_num * 2) #Used the variable to do maths
+print(str(my_num)) # Turns the number into a string
+print(str(my_num)+ " my favorite number") # So you can add a number in a string because otherwise you can't
+
+## TODO using maths functions
+
+print(abs(my_num)) # Gives the absolute number of my variable
+print(pow(3, 2)) # Gives power to the first number with the second eg. 3^2 une puissance 3 puissance 2
+print(max(3, 6)) # Gives the bigger number of the function
+print(min(3, 6)) # Just do the opposite of above
+print(round(4.9)) # Rounds a number following the basics rules
