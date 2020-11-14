00076303160476db99a8217f8108e09353e972c5
DoubleHfr <hugo.bruneau@gmail.com>
2020-11-14 16:18:49 +0100

    writting a basic calculator

---
+++ basic_calculator.py
@@ -0,0 +1,9 @@
+"""Building a basic calculator"""
+
+# I'm following this tutorial : https://www.youtube.com/watch?v=rfscVS0vtbw&t=52s&ab_channel=freeCodeCamp.org
+
+num_1 = input("Enter a number: ")
+num_2 = input("Enter another number: ")
+result = float(num_1) + float(num_2) # float function turn a string (between the parenthesis) into a number 
+
+print(result)
