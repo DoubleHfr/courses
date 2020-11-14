65ec208328c625d00cb27395199537b43d0dfbe8
DoubleHfr <hugo.bruneau@gmail.com>
2020-11-14 16:18:28 +0100

    I learn how to get inputs from users

---
+++ course_4_get_input_from_users.py
@@ -0,0 +1,17 @@
+"""getting inputs from users"""
+
+# I watch this course : https://www.youtube.com/watch?v=rfscVS0vtbw&t=52s&ab_channel=freeCodeCamp.org
+
+#input() # Tells to python "Hey I want to have an input from a user"
+#input("Enter your name") # Added a prompt to the line above !
+
+## TODO Write a basic program where the user enters his name and the computer says Hi !
+
+#name = input("Enter your name ! :") # Stock the input in a variable called name 
+#print("Hi " + name + "!") #Prints the name given by the user plus an "!"
+
+## TODO Complexify
+name = input("Enter your name ! :") # Stock the input in a variable called name 
+age = input("Enter your age ! :") # Stock the input in a variable called name 
+
+print("Hello " + name + "!" +  "\nYour are " + age + "." + "\nCongratulations !")
