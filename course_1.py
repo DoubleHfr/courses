c7fbfade4537ea59fe6c5df3ce7bbf5b20621b6d
DoubleHfr <hugo.bruneau@gmail.com>
2020-11-14 16:17:03 +0100

    My first course !

---
+++ course_1.py
@@ -0,0 +1,15 @@
+"""my first course of python"""
+
+# I watched this course : https://www.youtube.com/watch?v=rfscVS0vtbw&t=52s&ab_channel=freeCodeCamp.org
+
+# Global variables : 
+## There's three types of values :
+
+character_name = "Georges" # String which is plain text. Always written with ""
+character_age = 50 # Numbers which can be also decimal numbers eg. 50.56
+is_male = False # Boulean which is a true or false value to something (usefull in programming.)
+
+print("Un homme s'appelle " + character_name +"")
+print("Il est un peu vieux")
+#print(""+ character_name +"à"+ ((str(character_age""))
+print("" + character_name +" aime les bannanes")
