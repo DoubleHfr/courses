dd277dec08d4ddbc2da19ea55fe78faea0213fba
DoubleHfr <hugo.bruneau@gmail.com>
2020-11-15 12:04:44 +0100

    What's a tuple ?

---
+++ course_7_tuples.py
@@ -0,0 +1,15 @@
+""" What's tuples"""
+
+# I watch this course : https://www.youtube.com/watch?v=rfscVS0vtbw&t=52s&ab_channel=freeCodeCamp.org
+
+# Tuples are containers where you can store differents values
+## Same as a list but a tuple is UNMUTABLE it can't be changed
+
+## TODO How to create a Tuple ? 
+
+coordinates = (4, 5) # This is a tuple because of the parenthesis
+coordinates_2 = [4,5] # This is a list because of the square brackets
+
+## But you can create a list of tuples 
+
+coordinates_list = [(4, 5), (5, 7), (10, 8)] # Just as this !
