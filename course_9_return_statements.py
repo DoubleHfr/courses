a3330b36f7a0ba38a3cebcf66924e6ddd074e760
DoubleHfr <hugo.bruneau@gmail.com>
2020-11-15 12:48:35 +0100

    learn return statements

---
+++ course_9_return_statements.py
@@ -0,0 +1,15 @@
+"""return statements"""
+
+# I watch this course : https://www.youtube.com/watch?v=rfscVS0vtbw&t=52s&ab_channel=freeCodeCamp.org
+
+
+## The return keyword is used with a function in order to have communication back
+
+
+def cube(number):
+   return number*number*number
+    
+result = cube(4)
+print(result)
+
+## NOTHING GOES BELOW A RETURN STATEMENT IN THE FUNCTION INDENT
